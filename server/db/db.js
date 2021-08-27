303c82c1db26adb50f0a13f74f0af38182261449
Bertuskha <aruizvives@gmail.com>
2021-08-27 10:39:28 +0200

    database implementation with postgresql, take: 1

--- server/db/db.js
+++ server/db/db.js
@@ -2,12 +2,31 @@ import {Sequelize} from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config({path: '../config/.env'});
 
-const sequelize = new Sequelize ('forestryData', 'root', 'M4tryo.shka', {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+/**const sequelize = new Sequelize ('forestryData', 'root', 'M4tryo.shka', {
     host: 'https://forestry-app.herokuapp.com/',
     dialect: 'mysql',
     define: {
         timestamps: false
     },
 });
-
+*/
 export default sequelize;
