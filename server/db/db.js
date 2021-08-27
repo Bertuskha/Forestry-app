2b2106ced0bac174600ec07c3555fb52f7cdd72f
Bertuskha <aruizvives@gmail.com>
2021-08-27 13:13:55 +0200

    database implementation with postgresql, take: 4

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ import {Sequelize} from 'sequelize';
 import Client from 'pg';
 import dotenv from 'dotenv';
 dotenv.config({path: '../config/.env'});
-/**
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
       ssl: {
@@ -12,10 +12,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     }
   }
 );
-*/
+/**
 const sequelize = new Sequelize ('d9qfltv726qbf5', 'svptpogaockioz', 'cdc316687e5dde0b63746f5c3a0ea095fe786280dd82c7b225543d251d9b10f1', {
     host: 'ec2-35-153-114-74.compute-1.amazonaws.com:5432',
-    dialect: 'postgres',
+    dialect: 'postgres',    
     dialectOptions: {
         ssl: {
           require: true,
@@ -26,7 +26,7 @@ const sequelize = new Sequelize ('d9qfltv726qbf5', 'svptpogaockioz', 'cdc316687e
         timestamps: false
     },
 });
-
+*/
 
 sequelize
   .authenticate()
