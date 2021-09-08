39a1b2723ff5d6d61cd6b6b1cbea6ef00c6bd5c5
Bertuskha <aruizvives@gmail.com>
2021-09-08 12:17:06 +0200

    progress management

--- client/formpage.js
+++ client/formpage.js
@@ -66,8 +66,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
     async function getImages(){
-        leftIMGContainer.src = "";
-        rightIMGContainer.src = "";
         let images = {imageIndex: imageIndex};
         let response = await fetch('https://forestry-app.herokuapp.com/api/getImages', {
             method: "POST",
