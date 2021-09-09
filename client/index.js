b356824a7a18963a6febcfded82a47b399bb1a16
Bertuskha <aruizvives@gmail.com>
2021-09-09 13:46:04 +0200

    custom ID changes

--- client/index.js
+++ client/index.js
@@ -1,17 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     
+    //Fetches random user ID token from server
+    //Pre: true
+    //Post: Random token is saved in session storage and shown on screen
     async function getToken() {
         const response = await fetch('https://forestry-app.herokuapp.com/api/getToken');
         const userID = await response.text();
         sessionStorage.setItem('token', userID);
         document.getElementById("token").innerHTML = "User ID: " + userID;
     } 
+    getToken();
 
+    //If the user hasn't started the form, the question index is set to the first position
     if(sessionStorage.getItem('started') == null){
         sessionStorage.setItem('index', 1);
     }
-    getToken();
 
+    //Fetches the number of questions that are stored in the form's question database
+    //Pre: true
+    //Post: The total number of questions of the form is saved in the session storage
     async function getNumImages() {
         const response = await fetch('https://forestry-app.herokuapp.com/api/getNumImages');
         const result = await response.text();
@@ -19,11 +26,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }  
     getNumImages();
 
-    
-    const customID = document.getElementById("customID");
-    const btn = document.getElementById("Begin");
+    const customID = document.getElementById("customID"); //custom user ID input element
+    const btn = document.getElementById("Begin");         //Button that starts the form
     btn.onclick = function(){
-        if(customID.value === '' || customID.value == null){
+        if(customID.value === '' || customID.value == null || customID.value.size != 6){
             console.log("normal ID");
         }
         else{
